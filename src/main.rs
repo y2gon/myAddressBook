fdb0b4e47f36e2e4ea820312b30a5beba40938b0
y2gon <y2gon2@naver.com>
2022-01-26 17:38:51 +0900

    updating a file read/write

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,46 @@
 #![allow(non_snake_case)]
 #![allow(unused)]
 
+use std::fs;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{self, Write, Read};
 use std::str;
 
-/* 파일 입출력 */
-
-/* 저장될 Data 를 정의한 구조체 및 관련 methods */
+/* ----- 저장될 Data 를 정의한 구조체 및 관련 methods  ------ */
 
 struct PersonalInfo 
 {
-    // id : u32,
-    name : String,    // &str 로 선언할 경우 lifetime 문제 발생. &str 은 sliced array[u8] dlrh
-    address : String, // String 은 Vec<u8> 인데... array 타입이 오히려 길이가 정해져 있으므로 stack 에 저장되어 lifetime 문제가 없어야 되는거 아닌지..
+    name : String,  
+    address : String, 
 }
 
 impl PersonalInfo
 {
+    //static method (해당 구조체 instance 생성자)
+    pub fn newPerson(inputName: String, inputAddress : String) -> Self // method  return : Self struct 명(PersonalInfo))과 동일 
+    {
+        PersonalInfo{name:inputName, address:inputAddress}  // Self{} 로도 표현 가능
+     }
+
     pub fn showPersonalInfo(&self)
     {
-        println!("[NAME : {}  / ADDRESS : {}]", &self.name, &self.address);  // {}, 와 {:?} 차이는??
-        // -> The question mark operator (?) unwraps valid values or returns errornous values, propagating them to the calling function. 
-        // -> {:?} 로 사용되면 해당 출력 형태가 아닌 데이터 형태 그대로 출력 가능하게 해줌??? 
+        println!("[NAME : {}  / ADDRESS : {}]", &self.name, &self.address);  
     }
 }
 
-/* 연락처 Data 를 저장, 관리에 필요한 구조체 정의 및 methods */
-pub struct AllPersonalInfo
+/* ----- 연락처 Data 를 저장, 관리에 필요한 구조체 정의 및 methods ----- */
+pub struct GroupInfo
 {
-    allDatas : Vec<PersonalInfo>,
+    groupDatas : Vec<PersonalInfo>,
 }
 
-impl AllPersonalInfo
+impl GroupInfo
 {
     // 목록에 이름을 추가
     pub fn addPersonalInfo(&mut self, inputName:String, inputAddress:String)
     {
-        self.allDatas.push(PersonalInfo{name:inputName, address:inputAddress});
+        self.groupDatas.push(PersonalInfo::newPerson(inputName, inputAddress));
     }
 
     // (동명이인이 없다고 가정) 이름으로 검색하여 삭제하기 
@@ -46,7 +48,7 @@ impl AllPersonalInfo
     {
         let mut searchingStatus = false;
         let mut cnt:usize = 0;
-        for person in self.allDatas.iter()
+        for person in self.groupDatas.iter()
         {
             if person.name == inputName
             {
@@ -58,7 +60,7 @@ impl AllPersonalInfo
 
         if searchingStatus
         {
-            self.allDatas.remove(cnt); // vector index 값을 찾는 method 를 못찾아서 위 반복문을 통해 얻음.
+            self.groupDatas.remove(cnt); // vector index 값을 찾는 method 를 못찾아서 위 반복문을 통해 얻음.
             println!("[{}] 's infomation is removed in this group.", inputName);
         }
         else // 삭제 하고자 하는 이름이 vector 내에 없을 경우
@@ -72,7 +74,7 @@ impl AllPersonalInfo
     {
         let mut searchingStatus = false;
 
-        for person in self.allDatas.iter()
+        for person in self.groupDatas.iter()
         {
             if person.name == inputName
             {
@@ -89,41 +91,90 @@ impl AllPersonalInfo
     }
 
     // 현재 목록에 등록된 모든 이름 출력
-    pub fn showAllPersonalInfo(&self)
+    pub fn showGroupInfo(&self)
     {
-        for data in self.allDatas.iter()
+        for data in self.groupDatas.iter()
         {
             data.showPersonalInfo();
         }
     }
+
+    // 입력된 file 에 연락처 저장 ()
+    pub fn addGroupInfoToFile(&self, fileName:&str)
+    {
+        let mut file = OpenOptions::new().append(true).open(fileName);
+        let mut file = match file{
+            Ok(File) => File,
+            Err(error) => panic!("There was a problem opening the file: {:?}", error),
+        };
+        for data in self.groupDatas.iter()
+        {
+            let writingInfo = format!("{}\t{}\n", data.name, data.address);
+            file.write_all(writingInfo.as_bytes()).expect("Writing error");
+        }
+        
+    }
+
+    // 저장된 txt file 을 읽어서 해당 data 를 연락처 vector 값으로 저장
+    pub fn readGroupInfoFromFile(&mut self, fileName:&str)
+    {
+        let mut contents = fs::read_to_string(fileName);
+        let mut contents = match contents{
+            Ok(String) => String,
+            Err(error) => panic!("There was a problem opening the file: {:?}", error),
+        };
+        while contents.chars().count() > 0
+        {
+            let cuttingIdx1 = contents.find('\t').unwrap();
+            let cuttingIdx2 = contents.find('\n').unwrap();
+            let name = contents[..cuttingIdx1].to_string();
+            let address = contents[cuttingIdx1+1..cuttingIdx2].to_string();
+            self.addPersonalInfo(name, address);
+
+            contents = contents[cuttingIdx2+1..].to_string();
+        }
+        
+    }
 }
+    // Vector 내 저장된 data 모두 삭제 -> std method 에 대한 trait 사용 공부 필요
+    // trait Clear{}
+    // pub fn clearGroupInfo(&self) {}
 
+/* ----- MAIN ----- */
 fn main() {
-
-    // PersonalInfo 구조체 연습
-    let person1 = PersonalInfo{name:"QQ".to_string(), address:"Jeju".to_string()};
-    let person2 = PersonalInfo{name:"WW".to_string(), address:"Dokdo".to_string()};
-
-    person1.showPersonalInfo();
-    person2.showPersonalInfo();
-    // --------------------------------------------------------------------
-    let mut group1 = AllPersonalInfo{allDatas: Vec::new()};
+    /* ----- 연락처 그룹 vector 생성 및 data 입력 ----- */
+    let mut group1 = GroupInfo{groupDatas: Vec::new()};
 
     group1.addPersonalInfo("HJ".to_string(), "USA".to_string());
     group1.addPersonalInfo("DH".to_string(), "UK".to_string());
     group1.addPersonalInfo("JM".to_string(), "France".to_string());
     group1.addPersonalInfo("YG".to_string(), "Korea".to_string());
    
-    group1.showAllPersonalInfo();
+    group1.showGroupInfo();
     group1.searchingPersonalInfo("YG".to_string());
     group1.searchingPersonalInfo("aa".to_string());
     group1.removePersonalInfo("YG".to_string());
-    group1.showAllPersonalInfo();
+    group1.showGroupInfo();
     group1.searchingPersonalInfo("YG".to_string());
+
+    /* ----- txt file 에 해당 data 저장 ----- */
+    let fileName  = "addressBook.txt";
+    group1.addGroupInfoToFile(fileName);
+
+    /* ----- txt file 에 해당 data 저장 ----- */
+    println!("------- readGroup---------");
+    let mut readGroup = GroupInfo{groupDatas:Vec::new()};
+    readGroup.readGroupInfoFromFile(&fileName);
+    readGroup.showGroupInfo();
 
 }
 
 
+
+
+
+/* -----  ----- */
+/* -----  ----- */
 
 
 /*  아래 코드는 계속적으로 정리 필요 (파일 입출력 관련)
